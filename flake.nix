dceab553285919aae5b2195feebbc29febcb8f63
AoJ <aoj@n13.cz>
2023-02-19 18:31:40 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
         ({modulesPath, ... }: {
           imports = [
             (modulesPath + "/installer/scan/not-detected.nix")
-            (modulesPath + "/profiles/qemu-guest.nix")
+            ("./networking.nix")
             disko.nixosModules.disko
           ];
           disko.devices = import ./disk-config.nix {
@@ -25,8 +25,8 @@
           services.openssh.enable = true;
 
           users.users.root.openssh.authorizedKeys.keys = [
-            # change this to your ssh key
-            "CHANGE"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPnNW8yO3PT7c4QPxhLhKi3pxOKghyjPJ3Lk+piPwgTW aoj@mac13"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMgO3NYNEVQCQUzViAKoBjOSERArP6FpLVVSydH+nXC1 aoj@k.nxiii.cc"
           ];
         })
       ];
